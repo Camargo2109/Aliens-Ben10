c386614cedfa1425c442db4cb6e808e2ac27a0c4
Arthur Martins De Camargo <arthur.barrabonita@gmail.com>
2023-05-02 21:30:28 -0300

    ToLower Aliens.cs

--- Aliens-Ben10/Models/aliens.cs
+++ Aliens-Ben10/Models/aliens.cs
@@ -1,14 +1,18 @@
 
-namespace Aliens_Ben10.Models
+namespace Aliens_Ben10.Models;
+public class Aliens10 
 {
-    public class Aliens10
+    //Atributos
+     public int Numero {get; set;}
+    public string Nome {get; set;}
+    public string Descricao {get; set;}
+    public string Especie {get; set;}
+    public List<Tipo> Tipos {get; set;}
+    public string Imagem{get; set;}
+    
+            public Aliens10()
     {
-        //Atributos
-        public int Numero {get; set;}
-        public string Nome {get; set;}
-        public string Descricao {get; set;}
-        public string Especie {get; set;}
-        public  List<string>Tipo {get; set;}
-        public string Imagem{get; set;}
+            Tipo = new List<string>();
     }
-}
+
+}   
